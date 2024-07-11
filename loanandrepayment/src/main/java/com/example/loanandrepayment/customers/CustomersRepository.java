c035210c32f56d331ebe8530bcae6f9b1af820f7
muronji <muronjimulievi@gmail.com>
2024-07-11 11:44:31 +0300

    refactoring targets folder

--- loanandrepayment/src/main/java/com/example/loanandrepayment/customers/CustomersRepository.java
+++ loanandrepayment/src/main/java/com/example/loanandrepayment/customers/CustomersRepository.java
@@ -6,11 +6,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CustomersRepository extends JpaRepository<Customers, Long> {
 }
-package com.example.loanandrepayment.customers;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomersRepository extends JpaRepository<Customers, Long> {
-}
